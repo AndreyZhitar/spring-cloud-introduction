48f460c30cd884db64e9e0f9149dca72ad2fc46a
AndreyZhitar <Andreyzhitar1106@gmail.com>
2021-03-21 19:23:43 +0200

    zuul-api-gateway-server and sleuth zipkin

---
+++ zuul-api-gateway-server/src/main/java/com/zhitar/zuulapigatewayserver/filter/LoggingFilter.java
@@ -0,0 +1,35 @@
+package com.zhitar.zuulapigatewayserver.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+@Slf4j
+public class LoggingFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        log.info("request is -> {} request uri is -> {}", request, request.getRequestURI());
+        return null;
+    }
+}
