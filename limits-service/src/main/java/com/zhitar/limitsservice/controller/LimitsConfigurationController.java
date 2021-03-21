4d8097a22a4a38c3017b3ccd60c7bd3e23da2a7d
AndreyZhitar <Andreyzhitar1106@gmail.com>
2021-03-21 22:32:23 +0200

    hystrix fault tollerance

--- limits-service/src/main/java/com/zhitar/limitsservice/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/zhitar/limitsservice/controller/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.zhitar.limitsservice.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.zhitar.limitsservice.model.LimitsConfiguration;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,5 +15,15 @@ public class LimitsConfigurationController {
     @GetMapping("limits")
     public LimitsConfiguration retrieveLimitsFromConfiguration() {
         return new LimitsConfiguration(limitsConfiguration);
+    }
+
+    @GetMapping("fault-tolerance-example")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieveConfiguration")
+    public LimitsConfiguration retrieveConfiguration() {
+        throw new RuntimeException("Not available");
+    }
+
+    public LimitsConfiguration fallbackRetrieveConfiguration() {
+        return new LimitsConfiguration(50, 2);
     }
 }
