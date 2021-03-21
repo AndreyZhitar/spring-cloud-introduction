333c0b1bf012cfc014a4098e12f3b36d1b23dedb
AndreyZhitar <Andreyzhitar1106@gmail.com>
2021-03-21 14:36:23 +0200

    register as service discovery

--- currency-conversion-service/src/main/java/com/zhitar/currencyconversionservice/DemoApplication.java
+++ currency-conversion-service/src/main/java/com/zhitar/currencyconversionservice/DemoApplication.java
@@ -2,10 +2,12 @@ package com.zhitar.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.zhitar.currencyconversionservice")
+@EnableDiscoveryClient
 public class DemoApplication {
 
     public static void main(String[] args) {
